9d76559081849e9994056265c541b4c89758ba0d
최준용 <ChoiJunYong@choejun-yong-ui-MacBook-Pro.local>
2020-10-06 21:25:48 +0900

    -기본설정

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+
+module.exports = {
+    mode:'development',
+    entry: './main.js',
+    context: path.resolve(__dirname),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: 'pathOrUrlWhenProductionBuild'
+    },
+    module: {
+        rules: [
+            {test:/\.vue$/,
+            loader:'vue-loader',}
+        ]
+    },
+    resolve: {
+    },
+    devtool: 'source-map',
+    plugins: [
+        new VueLoaderPlugin(),
+    ]
+};
